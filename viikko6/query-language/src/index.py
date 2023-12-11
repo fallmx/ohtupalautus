b3df20d861e459c19985f0e975d7920e96a7ffa3
fallmx <31561277+fallmx@users.noreply.github.com>
2023-12-11 22:08:47 +0200

    Viikko 6 tehtävä 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,16 +1,20 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+    query = QueryBuilder()
+
+    matcher = (
+      query
+      .playsIn("NYR")
+      .hasAtLeast(10, "goals")
+      .hasFewerThan(20, "goals")
+      .build()
     )
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -57,3 +57,19 @@ class Or:
                 return True
 
         return False
+
+class QueryBuilder:
+    def __init__(self, matcher = All()):
+        self.matcher = matcher
+    
+    def playsIn(self, team):
+        return QueryBuilder(And(self.matcher, PlaysIn(team)))
+    
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.matcher, HasAtLeast(value, attr)))
+    
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.matcher, HasFewerThan(value, attr)))
+    
+    def build(self):
+        return self.matcher
